65c113b81b3e3eda62f3114844ce9a11e2702b65
diws <121597581+diws1@users.noreply.github.com>
2024-02-29 15:49:53 +0700

    Delete .scripts directory

--- .scripts/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# simple scripts for volume (with pipewire)
-# created by diws
-
-Icon=""
-Volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{ print $2*100"%" }')
-
-echo "$Icon $Volume"
